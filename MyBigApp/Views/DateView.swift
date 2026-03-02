9ab331d395877da5a72ddb4c03d09a77a1c8d6f8
Ann <ahu@lcs.on.ca>
2026-03-02 15:33:44 -0500

    starting toolbar

--- MyBigApp/Views/DateView.swift
+++ MyBigApp/Views/DateView.swift
@@ -9,113 +9,124 @@ import SwiftUI
 
 struct DateView: View {
     var body: some View {
-        HStack {
-            VStack{
-                HStack{
-                    Text("7:00")
-                        .font(.system(size:30.0))
+        NavigationStack{
+            HStack {
+                VStack{
+                    HStack{
+                        Text("7:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("8:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("8:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("9:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("9:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("10:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("10:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("11:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("11:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("12:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("12:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("13:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("13:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("Vitoria")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("14:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("14:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("Coco")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("15:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("15:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("Ann")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-                Spacer()
-                HStack{
-                    Text("16:00")
-                        .font(.system(size:30.0))
+                    HStack{
+                        Text("16:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                     Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
+                    HStack{
+                        Text("17:00")
+                            .font(.system(size:30.0))
+                        Spacer()
+                        Text("available")
+                            .font(.system(size:30.0))
+                        Image(systemName: "chevron.right")
+                    }
                 }
-                Spacer()
-                HStack{
-                    Text("17:00")
-                        .font(.system(size:30.0))
-                    Spacer()
-                    Text("available")
-                        .font(.system(size:30.0))
-                    Image(systemName: "chevron.right")
-                }
-            }
-        }
-        .padding()
                 
             }
+            .padding()
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading){
+                    Button{
+                        
+                    }label: {
+                        Image(systemName: "chevron.left")
+                    }
+                }
+            }
         }
-
-#Preview {
-    DateView()
+    }
 }
+    
+    #Preview {
+        DateView()
+    }
