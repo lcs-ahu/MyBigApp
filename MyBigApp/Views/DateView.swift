55312c8b590e08d56eae3228f33719de8a9b77b6
Ann <ahu@lcs.on.ca>
2026-03-02 14:53:36 -0500

    list set

---
+++ MyBigApp/Views/DateView.swift
@@ -0,0 +1,121 @@
+//
+//  ContentView.swift
+//  MyBigApp
+//
+//  Created by ANN HU on 2026/3/2.
+//
+
+import SwiftUI
+
+struct DateView: View {
+    var body: some View {
+        HStack {
+            VStack{
+                HStack{
+                    Text("7:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("8:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("9:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("10:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("11:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("12:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("13:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("14:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("15:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("16:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+                Spacer()
+                HStack{
+                    Text("17:00")
+                        .font(.system(size:30.0))
+                    Spacer()
+                    Text("available")
+                        .font(.system(size:30.0))
+                    Image(systemName: "chevron.right")
+                }
+            }
+        }
+        .padding()
+                
+            }
+        }
+
+#Preview {
+    DateView()
+}
